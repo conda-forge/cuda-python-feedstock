3700e9b25e17d200790cac884d64ca8cbdab619a
Leo Fang <leof@nvidia.com>
2025-08-07 17:36:41 +0800

    Update test_cimport.pyx

--- recipe/test_cimport.pyx
+++ recipe/test_cimport.pyx
@@ -1,6 +1,6 @@
-from cuda.bindings.driver cimport CUresult
-from cuda.bindings.runtime cimport cudaError_t
-from cuda.bindings.nvrtc cimport nvrtcResult
+from cuda.bindings.cydriver cimport CUresult
+from cuda.bindings.cyruntime cimport cudaError_t
+from cuda.bindings.cynvrtc cimport nvrtcResult
 
 def test():
     print("ok:", <CUresult>0, <cudaError_t>0, <nvrtcResult>0)
