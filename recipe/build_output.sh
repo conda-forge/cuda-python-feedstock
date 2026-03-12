95ec7f39b970850a3cade5074cd8e1a7d809b0f1
Leo Fang <leof@nvidia.com>
2026-03-11 23:08:18 -0400

    WAR for cuda-bindings

--- recipe/build_output.sh
+++ recipe/build_output.sh
@@ -2,7 +2,7 @@
 
 DIR_NAME="$(echo $PKG_NAME | tr '-' '_')"
 cd $DIR_NAME
-if [[ ${DIR_NAME} == "cuda_python" ]]; then
+if [[ ${DIR_NAME} == "cuda_python" || ${DIR_NAME} == "cuda_bindings" ]]; then
   # The upstream setup.py calls get_version() without dist_name,
   # so only the generic SETUPTOOLS_SCM_PRETEND_VERSION is honored.
   export SETUPTOOLS_SCM_PRETEND_VERSION=${PKG_VERSION}
