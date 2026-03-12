dc0b9ba21e998ebfa24ec34645b30c8aac800290
Leo Fang <leof@nvidia.com>
2026-03-11 21:04:26 -0400

    try this

--- recipe/build_output.sh
+++ recipe/build_output.sh
@@ -2,4 +2,8 @@
 
 DIR_NAME="$(echo $PKG_NAME | tr '-' '_')"
 cd $DIR_NAME
+if [[ ${DIR_NAME} == "cuda_python" ]]; then
+  # Release archives do not contain .git metadata; provide the exact version to not choke setuptools-scm
+  export SETUPTOOLS_SCM_PRETEND_VERSION_FOR_CUDA_PYTHON=${PKG_VERSION}
+fi
 $PYTHON -m pip install . --no-deps -vv
