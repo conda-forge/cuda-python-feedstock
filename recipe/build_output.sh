96cc2ac097185882f7846b74fb567b576cbd173d
Daniel Ching <dching@nvidia.com>
2026-09-16 17:53:48 -0500

    BLD: Enable pip check

--- recipe/build_output.sh
+++ recipe/build_output.sh
@@ -4,9 +4,5 @@ export CUDA_HOME="${PREFIX}/${SUBDIR}"
 
 DIR_NAME="$(echo $PKG_NAME | tr '-' '_')"
 cd $DIR_NAME
-if [[ ${DIR_NAME} == "cuda_python" ]]; then
-  # The upstream setup.py calls get_version() without dist_name,
-  # so only the generic SETUPTOOLS_SCM_PRETEND_VERSION is honored.
-  export SETUPTOOLS_SCM_PRETEND_VERSION=${PKG_VERSION}
-fi
+export SETUPTOOLS_SCM_PRETEND_VERSION=${PKG_VERSION}
 $PYTHON -m pip install . --no-deps -vv
